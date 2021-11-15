c386e448b5cafc733117a60b296cd6e713ed1d88
tobibot <tobias.heying@outlook.com>
2021-11-15 14:23:57 +0100

    init

---
+++ csharpfeatures/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Person p = new ("Nathan", "Heying");
+
+Console.WriteLine($"Hello {p.FirstName} {p.LastName}");
